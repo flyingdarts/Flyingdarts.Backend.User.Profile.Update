47d76e5a30bf274c4a444c433e93a6b186206adf
Mike Pattyn <mike.pattyn1@gmail.com>
2023-05-20 11:49:21 +0200

    Only send if email is know verified

--- CQRS/UpdateUserProfileCommandSendVerificationEmailHandler.cs
+++ CQRS/UpdateUserProfileCommandSendVerificationEmailHandler.cs
@@ -18,6 +18,11 @@ namespace Flyingdarts.Backend.User.Profile.Update.CQRS
     {
         public async Task Process(UpdateUserProfileCommand request, APIGatewayProxyResponse response, CancellationToken cancellationToken)
         {
+            if (request.Email != "mike.pattyn1@gmail.com" && 
+                request.Email != "support@flyingdarts.net" && 
+                request.Email != "mike@flyingdarts.net")
+                return;
+
             var sqsClient = new AmazonSQSClient();
             var messageRequest = new SendMessageRequest
             {
