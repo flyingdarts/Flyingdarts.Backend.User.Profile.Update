7d4dbf157b8400d400721782d616491bf7f8a1ec
daniela <whateverellie@gmail.com>
2023-05-29 14:48:53 +0200

    Update by UserId

--- CQRS/UpdateUserProfileCommand.cs
+++ CQRS/UpdateUserProfileCommand.cs
@@ -4,10 +4,8 @@ using MediatR;
 public class UpdateUserProfileCommand : IRequest<APIGatewayProxyResponse>
 {
     public string UserId { get;set;}
-    public string CognitoUserId { get; set; }
-    public string CognitoUserName { get; set; }
     public string UserName { get; set; }
     public string Email { get; set; }
     public string Country { get; set; }
-    public string ConnectionId { get; set; }
+    internal string ConnectionId { get; set; }
 }
